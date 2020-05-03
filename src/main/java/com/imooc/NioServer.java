a3b9acbb502f391221e3e1d73056d8b1aae5174a
huang zexin <342756877@qq.com>
2020-05-02 20:37:27 -0400

    finish server

--- src/main/java/com/imooc/NioServer.java
+++ src/main/java/com/imooc/NioServer.java
@@ -2,9 +2,12 @@ package com.imooc;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -81,18 +84,98 @@ public class NioServer {
                 /**
                  * 如果是 接入事件
                  */
-
+                if(selectionKey.isAcceptable()){
+                    acceptHandler(serverSocketChannel,selector);
+                }
                 // do somethings
 
                 /**
                  * 如果是可读事件
                  */
+                if(selectionKey.isReadable()){
+                    readHandler(selectionKey,selector);
+                }
                 // do somethings
             }
         }
 
 
     }
+
+    /**
+     * 接入事件处理器
+     */
+    private void acceptHandler(ServerSocketChannel serverSocketChannel,Selector selector) throws IOException {
+        /**
+         * 如果是接入事件，创建socketchannel
+         */
+        SocketChannel socketChannel = serverSocketChannel.accept();
+
+        /**
+         * 将socketChannel 设置为非阻塞工作模式
+         */
+        socketChannel.configureBlocking(false);
+        /**
+         * 将channel注册到selector上，监听可读事件
+         */
+        socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+
+        /**
+         * 回复客户端提示信息
+         */
+        socketChannel.write(Charset.forName("UTF-8").encode("你与聊天室其他人都是不是朋友关系，请注意隐私安全"));
+
+    }
+
+    /**
+     * 可读事件处理
+     */
+    private void readHandler(SelectionKey selectionKey,Selector selector) throws IOException {
+        /**
+         * 要从 selectionKey中获取到已经就绪的channel
+         */
+
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+        /**
+         * 创建buffer
+         */
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+
+        /**
+         * 循环读取客户端请求信息
+         */
+        String request = "";
+        while(socketChannel.read(byteBuffer)>0){
+            /**
+             * 切换buffer为读模式
+             */
+            byteBuffer.flip();
+
+            /**
+             * 读取buffer中的内容
+             */
+            request += Charset.forName("UTF-8").decode(byteBuffer);
+        }
+
+        /**
+         * 将channel再次注册到selector上，监听他的可读信息
+         */
+        socketChannel.register(selector,SelectionKey.OP_READ);
+
+        /**
+         * 将客户端发送的请求信息广播给其他客户端
+         */
+        if(request.length()>0){
+            //广播给其他客户端
+            System.out.println("::"+request);
+        }
+
+    }
+
 
     public static void main(String[] args) {
         NioServer nioServer = new NioServer();
